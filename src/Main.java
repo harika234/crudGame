aba2f658083e51d9d28241e4e7e915a468309eaf
kcterala <kcterala@gmail.com>
2023-11-03 01:09:01 +0530

    refactor: spaces and indentations

--- src/Main.java
+++ src/Main.java
@@ -8,33 +8,27 @@ public class Main {
         printOperations();
 
         int n1 = scn.nextInt();
-        while(n1!=0) {
+        while(n1 != 0) {
             if(n1 == 1){
                 System.out.println("please enter 2 numbers for addition.");
                 int add1 = scn.nextInt();
                 int add2 = scn.nextInt();
                 int result = add1+add2;
-                System.out.println("hey the result of addition is - "+ result);
+                System.out.println("hey the result of addition is "+ result);
             }
             if(n1 == 2){
                 System.out.println("please enter 2 numbers for subtraction.");
                 int sub1 = scn.nextInt();
                 int sub2 = scn.nextInt();
-                int result = Math.abs(sub1-sub2);
+                int result = Math.abs(sub1 - sub2);
                 System.out.println("hey the result of subtraction is "+ result);
             }
 
             if(n1 == 3) {
-                int result = 0;
                 System.out.println("please enter 2 numbers for Multiplication.");
                 int mul1 = scn.nextInt();
                 int mul2 = scn.nextInt();
-                if(mul1==0 || mul2==0) {
-                    result = 0;
-                }
-                else{
-                    result = mul2*mul1;
-                }
+                int result = mul1 * mul2;
                 System.out.println("hey the result of multiplication is "+ result);
             }
 
@@ -42,19 +36,12 @@ public class Main {
                 System.out.println("please enter 2 numbers for division.");
                 int div1 = scn.nextInt();
                 int div2 = scn.nextInt();
-                int result  = 0;
-                if(div1>0 && div2>0) {
-                    result = div1/div2;
-                    System.out.println("hey the result of division  is "+ result);
-                }
-                if(div1 == 0) {
-                    result = 0;
-                    System.out.println("hey the result of division  is "+ result);
-                }
                 if(div2 == 0) {
                     System.out.println("the result is undefined!!");
+                } else {
+                    int result = div1 / div2;
+                    System.out.println("hey the result of division  is "+ result);
                 }
-
             }
             System.out.println("enter operation to perform");
 //            printOperations();
